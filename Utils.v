58233afe6d156a6070db8250b0fa71607d626903
Clastres Terence <terence.clastres@etu.univ-orleans.fr>
2023-03-18 22:02:25 +0100

    RAC of arithmetic properties: more notations, apply remarks from meetup

--- Utils.v
+++ Utils.v
@@ -8,7 +8,9 @@ Open Scope utils_scope.
 Definition partial_function {X Y : Type} := X -> option Y.
 Definition empty_p {X Y:Type} := fun (_:X) => (None: option Y).
 
+Infix "⨉" := prod (at level 99).
 Notation "⊥" := empty_p : utils_scope.
+Notation "'ℤ'" := Z (at level 99).
 
 Class EqDec X :=
 {
@@ -27,10 +29,23 @@ Definition p_map_addall {X Y: Type} `{EqDec X} env (l:list (X*Y)) :=
     List.fold_left (fun f (xy:X*Y) => let (x,y) := xy in f {x \ y}) l env
 .
 
+Definition in_domain { X Y : Type} (f: X ⇀ Y) (x:X) := (exists y, (f x) = Some y).
 
-Definition domain { X Y : Type} (f: X ⇀ Y) (x:X) := (exists y, (f x) = Some y) 
-.
-Notation "'dom' f" := (domain f) : utils_scope.
+(* fixme domain def here *)
+
+Notation "'dom' f" := (in_domain f) : utils_scope.
+Notation "x ∈ E" := (in_domain E x) (at level 99) : utils_scope.
+Notation "x ∉ E" := (~ in_domain E x) (at level 99) : utils_scope.
+
+Definition min_domain { X : Type} (dom1: X -> Prop) (dom2: X -> Prop) (x:X) := dom1 x /\ ~ dom2 x.
+Infix "-" := min_domain : utils_scope.
+
+
+Fact d_sub_d_empty {X Y : Type} : forall (f : (X ⇀ Y)),
+    ~ exists n, (dom f - dom f) n.
+Proof.
+    intros f contra. inversion contra. inversion H. destruct H1. assumption.
+Qed.
 
 
 Module Type Bounds.
@@ -47,5 +62,4 @@ Module MachineInteger(B:Bounds).
     Definition to_z (i:MI) : Z := i.(val).
     Definition mi_eqb m1 m2 := val m1 =? val m2.
 End MachineInteger.
-
 
